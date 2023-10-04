bdc1ec07919ac9812f7a9cae1b8f7966f881a682
pmosk <pm@ggroupp.ru>
2023-10-04 18:34:37 +0400

    Improve DateTimeOffset example

---
+++ src/core/Endpoint.Core/Endpoint.MetadataHelper/Schema/Schema.String.DateTimeOffset.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+
+namespace GarageGroup.Infra.Endpoint;
+
+partial class EndpointMetadataHelper
+{
+    public static OpenApiSchema CreateDateTimeOffsetSchema(bool nullable, IOpenApiAny? example = null, string? description = null)
+        =>
+        new()
+        {
+            Type = "string",
+            Format = "date-time",
+            Nullable = nullable,
+            Example = example ?? new OpenApiString(DateTimeOffset.Now.ToString("o")),
+            Description = description
+        };
+}
